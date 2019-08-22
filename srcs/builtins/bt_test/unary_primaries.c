12b11414041b6a1d909353803f72a71e9770532b
Antoine <aleduc@student.42.fr>
2019-08-22 19:50:27 +0200

    Finished Test BI, now need to test each options and fix the compiling bug

--- srcs/builtins/bt_test/unary_primaries.c
+++ srcs/builtins/bt_test/unary_primaries.c
@@ -1,8 +1,12 @@
 #include "builtins.h"
 
+/*
+** I need to do Read, Write, and Execute options (-r, -w, -x)
+** Except that everything else should work, but need to test each options
+*/
+
 int		b_test(char *str1, char *str2, char *str3)
 {
-<<<<<<< HEAD
 	struct stat	sb;
 
 	ft_putendl("je suis dans l'unary primary -b");
@@ -11,16 +15,13 @@ int		b_test(char *str1, char *str2, char *str3)
 		if ((sb.st_mode & S_IFMT) == S_IFBLK)
 			return (0);
 	}
-=======
 	(void)str1;(void)str2;(void)str3;
 	ft_putendl("je suis dans l'unary primary b");
->>>>>>> a50095f9b10dbc063aee2677d57814a18f143a97
 	return (1);
 }
 
 int		c_test(char *str1, char *str2, char *str3)
 {
-<<<<<<< HEAD
 	struct stat	sb;
 
 	ft_putendl("je suis dans l'unary primary -c");
@@ -29,16 +30,13 @@ int		c_test(char *str1, char *str2, char *str3)
 		if ((sb.st_mode & S_IFMT) == S_IFCHR)
 			return (0);
 	}
-=======
 	(void)str1;(void)str2;(void)str3;
 	ft_putendl("je suis dans l'unary primary c");
->>>>>>> a50095f9b10dbc063aee2677d57814a18f143a97
 	return (1);
 }
 
 int		d_test(char *str1, char *str2, char *str3)
 {
-<<<<<<< HEAD
 	struct stat	sb;
 
 	ft_putendl("je suis dans l'unary primary -d");
@@ -47,31 +45,25 @@ int		d_test(char *str1, char *str2, char *str3)
 		if ((sb.st_mode & S_IFMT) == S_IFDIR)
 			return (0);
 	}
-=======
 	(void)str1;(void)str2;(void)str3;
 	ft_putendl("je suis dans l'unary primary d");
->>>>>>> a50095f9b10dbc063aee2677d57814a18f143a97
 	return (1);
 }
 
 int		e_test(char *str1, char *str2, char *str3)
 {
-<<<<<<< HEAD
 	struct stat	sb;
 
 	ft_putendl("je suis dans l'unary primary -e");
 	if (stat(str2, &sb) != -1)
 			return (0);
-=======
 	(void)str1;(void)str2;(void)str3;
 	ft_putendl("je suis dans l'unary primary e");
->>>>>>> a50095f9b10dbc063aee2677d57814a18f143a97
 	return (1);
 }
 
 int		f_test(char *str1, char *str2, char *str3)
 {
-<<<<<<< HEAD
 	struct stat	sb;
 
 	ft_putendl("je suis dans l'unary primary -f");
@@ -80,119 +72,122 @@ int		f_test(char *str1, char *str2, char *str3)
 		if ((sb.st_mode & S_IFMT) == S_IFREG)
 			return (0);
 	}
-=======
 	(void)str1;(void)str2;(void)str3;
 	ft_putendl("je suis dans l'unary primary f");
->>>>>>> a50095f9b10dbc063aee2677d57814a18f143a97
 	return (1);
 }
 
 int		g_test(char *str1, char *str2, char *str3)
 {
-<<<<<<< HEAD
+	struct stat	sb;
+
 	ft_putendl("je suis dans l'unary primary -g");
-=======
+	if (stat(str2, &sb) != -1)
+	{
+		if ((sb.st_mode & S_IFMT) == S_ISGID)
+			return (0);
+	}
 	(void)str1;(void)str2;(void)str3;
-	ft_putendl("je suis dans l'unary primary g");
->>>>>>> a50095f9b10dbc063aee2677d57814a18f143a97
 	return (1);
 }
 
 int		L_test(char *str1, char *str2, char *str3)
 {
-<<<<<<< HEAD
+	struct stat	sb;
+
 	ft_putendl("je suis dans l'unary primary -L");
-=======
+	if (stat(str2, &sb) != -1)
+	{
+		if ((sb.st_mode & S_IFMT) == S_IFLNK)
+			return (0);
+	}
 	(void)str1;(void)str2;(void)str3;
-	ft_putendl("je suis dans l'unary primary L");
->>>>>>> a50095f9b10dbc063aee2677d57814a18f143a97
 	return (1);
 }
 
 int		p_test(char *str1, char *str2, char *str3)
 {
-<<<<<<< HEAD
+	struct stat	sb;
+
 	ft_putendl("je suis dans l'unary primary -p");
-=======
+	if (stat(str2, &sb) != -1)
+	{
+		if ((sb.st_mode & S_IFMT) == S_IFIFO)
+			return (0);
+	}
 	(void)str1;(void)str2;(void)str3;
-	ft_putendl("je suis dans l'unary primary p");
->>>>>>> a50095f9b10dbc063aee2677d57814a18f143a97
 	return (1);
 }
 
 int		r_test(char *str1, char *str2, char *str3)
 {
-<<<<<<< HEAD
 	ft_putendl("je suis dans l'unary primary -r");
-=======
 	(void)str1;(void)str2;(void)str3;
 	ft_putendl("je suis dans l'unary primary r");
->>>>>>> a50095f9b10dbc063aee2677d57814a18f143a97
 	return (1);
 }
 
 int		S_test(char *str1, char *str2, char *str3)
 {
-<<<<<<< HEAD
+	struct stat	sb;
+
 	ft_putendl("je suis dans l'unary primary -S");
-=======
+	if (stat(str2, &sb) != -1)
+	{
+		if ((sb.st_mode & S_IFMT) == S_IFIFO)
+			return (0);
+	}
 	(void)str1;(void)str2;(void)str3;
-	ft_putendl("je suis dans l'unary primary S");
->>>>>>> a50095f9b10dbc063aee2677d57814a18f143a97
 	return (1);
 }
 
 int		s_test(char *str1, char *str2, char *str3)
 {
-<<<<<<< HEAD
+	struct stat	sb;
+
 	ft_putendl("je suis dans l'unary primary -s");
-=======
+	if (stat(str2, &sb) != -1)
+	{
+		if (sb.st_size > 0)
+			return (0);
+	}
 	(void)str1;(void)str2;(void)str3;
-	ft_putendl("je suis dans l'unary primary s");
->>>>>>> a50095f9b10dbc063aee2677d57814a18f143a97
 	return (1);
 }
 
 int		u_test(char *str1, char *str2, char *str3)
 {
-<<<<<<< HEAD
+	struct stat	sb;
+
 	ft_putendl("je suis dans l'unary primary -u");
-=======
+	if (stat(str2, &sb) != -1)
+	{
+		if ((sb.st_mode & S_IFMT) == S_ISUID)
+			return (0);
+	}
 	(void)str1;(void)str2;(void)str3;
-	ft_putendl("je suis dans l'unary primary u");
->>>>>>> a50095f9b10dbc063aee2677d57814a18f143a97
 	return (1);
 }
 
 int		w_test(char *str1, char *str2, char *str3)
 {
-<<<<<<< HEAD
 	ft_putendl("je suis dans l'unary primary -w");
-=======
 	(void)str1;(void)str2;(void)str3;
-	ft_putendl("je suis dans l'unary primary w");
->>>>>>> a50095f9b10dbc063aee2677d57814a18f143a97
 	return (1);
 }
 
 int		x_test(char *str1, char *str2, char *str3)
 {
-<<<<<<< HEAD
 	ft_putendl("je suis dans l'unary primary -x");
-=======
 	(void)str1;(void)str2;(void)str3;
-	ft_putendl("je suis dans l'unary primary x");
->>>>>>> a50095f9b10dbc063aee2677d57814a18f143a97
 	return (1);
 }
 
 int		z_test(char *str1, char *str2, char *str3)
 {
-<<<<<<< HEAD
 	ft_putendl("je suis dans l'unary primary -z");
-=======
+	if (ft_strlen(str2) == 0)
+		return (0);
 	(void)str1;(void)str2;(void)str3;
-	ft_putendl("je suis dans l'unary primary z");
->>>>>>> a50095f9b10dbc063aee2677d57814a18f143a97
 	return (1);
 }
